6f87143f75aee7cd17e4c2f331f4554c9cbb748e
Lotusfa <lotusfa.com@gmail.com>
2026-05-01 04:33:08 +0800

    index html order

--- cantonese/main.js
+++ cantonese/main.js
@@ -33,8 +33,8 @@ initIPAIndexPage({
   languageSelectorId: 'lang-selector-btn',
   footerToolsContainerId: 'footer-tools',
   toolsConfig: [
-    { id: 'ipa-list', icon: 'globe', label: 'IPA 資料庫', type: 'link', href: './ipa_list.html' },
     { id: 'share-btn', icon: 'share', label: '分享 (Share)', type: 'share', visible: 'after-translate' },
+    { id: 'ipa-list', icon: 'globe', label: 'IPA 資料庫', type: 'link', href: './ipa_list.html' },
     { id: 'game-btn', icon: 'gamepad', label: 'Quiz Game', type: 'game', visible: 'after-translate' },
   ]
 });
